7b530f340b713808211a6ddd9c6ff20db9cb543a
abirev <abinayarevathi96@gmail.com>
2022-11-23 17:07:39 +0530

    hi

---
+++ org/at/AREV.java
@@ -0,0 +1,24 @@
+package org.at;
+public class AREV {
+	public void abName() 
+	{
+		
+System.out.println("ABIRM");
+	}
+	private void abAge()
+	{
+	System.out.println("25");
+
+	}
+	public static void main(String[] args) 
+	{
+		AREV a=new AREV();
+		a.abAge();
+		a.abName();
+		
+	}
+	
+
+	}
+	
+
